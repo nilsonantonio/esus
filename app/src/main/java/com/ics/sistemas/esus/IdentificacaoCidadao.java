a874739e434815ee90813e30be70644ea39e048c
Nilson Antônio <nilson.antonio14@gmail.com>
2015-04-15 13:45:02 -0300

    comitando projeto

---
+++ app/src/main/java/com/ics/sistemas/esus/IdentificacaoCidadao.java
@@ -0,0 +1,16 @@
+package com.ics.sistemas.esus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by nilson on 13/04/15.
+ */
+public class IdentificacaoCidadao extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dados_pessoais);
+    }
+}
